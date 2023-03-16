8ca7e50f6eae5fa25870ee60683fec5aea1fe9e7
julianchatto <juc226@lehigh.edu>
2023-03-16 17:03:38 -0400

    made function autorun

--- public/displaying.js
+++ public/displaying.js
@@ -3,14 +3,12 @@ import courses from "../data/courses.js";
 const course = ["HELLO", "TEST", "TEST2", "TEST3"]; // for some reason courses ARRAY is not working properly
 const grid = document.querySelector(".grid");
 
-function addCourses() {
+(function addCourses() {
   courses.forEach((item) => {
     console.log(1);
     const box = document.createElement("li");
     box.classList.add("box");
-    box.innerText = item;
+    box.innerText = item.title;
     grid.appendChild(box);
   });
-}
-
-addCourses();
+})();
