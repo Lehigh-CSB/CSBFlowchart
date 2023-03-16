a65713bf8f4616a3c16879679a69f6b864ea551b
julianchatto <juc226@lehigh.edu>
2023-03-16 17:22:47 -0400

    added event listeners

--- public/displaying.js
+++ public/displaying.js
@@ -4,11 +4,46 @@ const course = ["HELLO", "TEST", "TEST2", "TEST3"]; // for some reason courses A
 const grid = document.querySelector(".grid");
 
 (function addCourses() {
-  courses.forEach((item) => {
+  course.forEach((item) => {
     console.log(1);
     const box = document.createElement("li");
     box.classList.add("box");
-    box.innerText = item.title;
+    box.innerText = item;
     grid.appendChild(box);
   });
 })();
+
+/*
+    Event Listeners for the boxes
+*/
+const box = document.querySelectorAll(".box");
+// Mouse enter event
+box.forEach((cell) => {
+  cell.addEventListener("mouseenter", () => {
+    cell.style.backgroundColor = "blue";
+  });
+});
+// Mouse leave event
+box.forEach((cell) => {
+  cell.addEventListener("mouseleave", () => {
+    cell.style.backgroundColor = "";
+  });
+});
+
+/*
+    Method to return a list of prerequisites for a given course
+    @param  testCourse: the course to find the prerequisites for
+    @return preReqs: a list of prerequisites for the given course
+*/
+function findPreReq(testCourse) {
+  // need work
+  //     let preReqs = [];
+  //     testCourse.forEach((item) => {
+  //         item.prereqs.forEach((item2) => {
+  //             if(item2 === courses) {
+  //                 preReqs.push(item);
+  //             }
+  //         });
+  //     });
+  //   return preReq;
+}
