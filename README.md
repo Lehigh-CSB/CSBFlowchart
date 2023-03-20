3ae3a2763afdfb856e992efcd23842921c5819c5
Hana <and224@lehigh.edu>
2023-03-19 20:37:00 -0400

    jointJS trial

--- README.md
+++ README.md
@@ -12,3 +12,7 @@
 * [JavaScript](https://www.linkedin.com/learning/paths/become-a-javascript-developer?u=2222241)
 
 * [React](https://www.linkedin.com/learning/paths/explore-react-js-development?u=2222241)
+ 
+#### Code Examples:
+* [Flowchart](https://www.jointjs.com/demos/flowchart)
+* [Documentation] (https://resources.jointjs.com/?_gl=1*oatt72*_ga*MTg4MDMxOTA5OS4xNjc5MjY3MTYw*_ga_S3PN155Z28*MTY3OTI3MTE2NS4yLjEuMTY3OTI3MTE3NC41MS4wLjA.&_ga=2.191247401.2050008376.1679267160-1880319099.1679267160)
--- public/script.js
+++ public/script.js
@@ -3,9 +3,10 @@
 const grid = document.querySelector(".grid");
 let courses = [];
 console.log("In client side");
+let graph;
 
-// make a get request to the server to get the courses and print them to the console
-// the server will send back a stringified version of the courses array
+// // make a get request to the server to get the courses and print them to the console
+// // the server will send back a stringified version of the courses array
 fetch("/courses")
   .then((response) => {
     return response.json();
@@ -14,55 +15,66 @@ fetch("/courses")
     console.log(data);
     // data is an array of courses
     courses = data;
-    addCourses();
+    // addCourses();
+    // renderCourses is put in the fetch function so that it will only run after the data is received
+    renderCourses();
   });
 
-function addCourses() {
-  // console.log("In addCourses function");
-  courses.forEach((item) => {
-    // console.log("In forEach loop");
-    // const box = document.createElement("li");
-    // box.classList.add("box");
-    // box.innerText = item.title;
-    // grid.appendChild(box);
+// function addCourses() {
+//   // console.log("In addCourses function");
+//   courses.forEach((item) => {
+//     grid.innerHTML += `<li class="box">${item.title}</li>`;
+//   });
+// }
 
-    // grid is <ul class="grid"></ul>
-    // add a <li> element to the grid for each course
-    grid.innerHTML += `<li class="box">${item.title}</li>`;
+// USE LIBRARY GOJS
+
+function renderCourses() {
+  // console.log("In renderCourses function");
+  
+  var namespace = joint.shapes;
+  graph = new joint.dia.Graph({}, { cellNamespace: namespace });
+
+  var paper = new joint.dia.Paper({
+      // el is the DOM element that will contain the paper
+      el: document.getElementById('myholder'),
+      // model is the graph that will be rendered inside the paper
+      model: graph,
+      // make paper fit the size of the container
+      gridSize: 1,
+      cellViewNamespace: namespace,
+      // make paper non-responsive to pointer events so that elements are not draggable
+      interactive: false
   });
+
+  let course = findCourseByTitle("CSE 007");
+  // create a rectangle for the course
+  createCourseBox(100, 30, course.title);
 }
 
-/*
-    Event Listeners for the boxes
-*/
-// const box = document.querySelectorAll(".box");
-// // Mouse enter event
-// box.forEach((cell) => {
-//   cell.addEventListener("mouseenter", () => {
-//     cell.style.backgroundColor = "blue";
-//   });
-// });
-// // Mouse leave event
-// box.forEach((cell) => {
-//   cell.addEventListener("mouseleave", () => {
-//     cell.style.backgroundColor = "";
-//   });
-// });
-
-/*
-    Method to return a list of prerequisites for a given course
-    @param  testCourse: the course to find the prerequisites for
-    @return preReqs: a list of prerequisites for the given course
-*/
-function findPreReq(testCourse) {
-  // need work
-  //     let preReqs = [];
-  //     testCourse.forEach((item) => {
-  //         item.prereqs.forEach((item2) => {
-  //             if(item2 === courses) {
-  //                 preReqs.push(item);
-  //             }
-  //         });
-  //     });
-  //   return preReq;
+function findCourseByTitle(title) {
+  return courses.find((course) => course.title === title);
 }
+
+function createCourseBox(xCoord, yCoord, title) {
+  var rect = new joint.shapes.standard.Rectangle();
+  rect.position(xCoord, yCoord);
+  rect.resize(100, 40);
+  rect.attr({
+      body: {
+          fill: 'black',
+          // set rectangle's border rounded
+          rx: 10,
+          ry: 10,
+      },
+      label: {
+          text: title,
+          fill: 'white',
+      }
+  });
+  // Set the draggable attribute to false
+  rect.attr('rect/draggable', false);
+  rect.addTo(graph);
+}
+
+
