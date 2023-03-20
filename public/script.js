07843fe35539c8362b8ab6b96fa39189b48a70b1
Hana <and224@lehigh.edu>
2023-03-20 12:57:44 -0400

    render all courses

--- public/script.js
+++ public/script.js
@@ -217,6 +217,62 @@ function renderCourses() {
 	}
 	drawLines();
 
+<<<<<<< HEAD
+=======
+  // for x-coordinate, the first one for that level is 100, the second one is 200, the third one is 300, etc.
+  let xCoordLevel1 = 100;
+  let xCoordLevel1_5 = 100;
+  let xCoordLevel2 = 100;
+  let xCoordLevel2_5 = 100;
+  let xCoordLevel3 = 100;
+  let xCoordLevel3_5 = 100;
+  let xCoordLevel4 = 100;
+
+  // traverse through array of courses and create a rectangle for each course
+  for (let i = 0; i < courses.length; i++) {
+    // createCourseBox(100, 30, courses[i].title);
+    // there are multiple levels of courses, render the y-coordinate of the rectangle based on the level
+    // level 1: 30, level 1.5: 45, level 2: 60, level 2.5: 75, level 3: 90, level 3.5: 105, level 4: 120
+    let yCoord;
+    if (courses[i].level === 1) {
+      yCoord = 50;
+      createCourseBox(xCoordLevel1, yCoord, courses[i].title);
+      // increment the x-coordinate for the next course at this level
+      xCoordLevel1 += 120;
+    } else if (courses[i].level === 1.5) {
+      yCoord = 150;
+      createCourseBox(xCoordLevel1_5, yCoord, courses[i].title);
+      // increment the x-coordinate for the next course at this level
+      xCoordLevel1_5 += 120;
+      createCourseBox(xCoordLevel1_5, yCoord, courses[i].title);
+    } else if (courses[i].level === 2) {
+      yCoord = 250;
+      createCourseBox(xCoordLevel2, yCoord, courses[i].title);
+      // increment the x-coordinate for the next course at this level
+      xCoordLevel2 += 120;
+    } else if (courses[i].level === 2.5) {
+      yCoord = 350;
+      createCourseBox(xCoordLevel2_5, yCoord, courses[i].title);
+      // increment the x-coordinate for the next course at this level
+      xCoordLevel2_5 += 120;
+    } else if (courses[i].level === 3) {
+      yCoord = 450;
+      createCourseBox(xCoordLevel3, yCoord, courses[i].title);
+      // increment the x-coordinate for the next course at this level
+      xCoordLevel3 += 120;
+    } else if (courses[i].level === 3.5) {
+      yCoord = 550;
+      createCourseBox(xCoordLevel3_5, yCoord, courses[i].title);
+      // increment the x-coordinate for the next course at this level
+      xCoordLevel3_5 += 120;
+    } else if (courses[i].level === 4) {
+      yCoord = 650;
+      createCourseBox(xCoordLevel4, yCoord, courses[i].title);
+      // increment the x-coordinate for the next course at this level
+      xCoordLevel4 += 120;
+    }
+  }
+>>>>>>> c6832e1 (render all courses)
 }
 
 // mouse enter event for each course card
@@ -278,6 +334,7 @@ function findCourseByTitle(title) {
   	return courses.find((course) => course.title === title);
 }
 
+<<<<<<< HEAD
 function createStart(x, y, title, color) {
 	let rect = new joint.shapes.standard.Rectangle({
 		position: { x, y },
@@ -317,6 +374,41 @@ function createStart(x, y, title, color) {
 	rect.prop('offered', course.offered);
 	rect.addTo(graph);
 	return rect;
+=======
+function createCourseBox(xCoord, yCoord, title) {
+  var rect = new joint.shapes.standard.Rectangle();
+  rect.position(xCoord, yCoord);
+  rect.resize(100, 40);
+  rect.attr({
+      body: {
+          fill: 'black',
+          // set rectangle's border rounded
+          rx: 10,
+          ry: 10,
+      },
+      label: {
+          text: title,
+          fill: 'white',
+      }
+  });
+  // Set the draggable attribute to false
+  rect.attr('rect/draggable', false);
+  // Set custom attributes based on the course
+  let course = findCourseByTitle(title);
+  rect.prop('title', course.title);
+  rect.prop('description', course.description);
+  rect.prop('credits', course.credits);
+  rect.prop('designation', course.designation);
+  rect.prop('completed', course.prerequisites);
+  rect.prop('used', course.used);
+  rect.prop('grade', course.grade);
+  rect.prop('level', course.level);
+  rect.prop('prereqs', course.prerequisites);
+  rect.prop('coreqs', course.corequisites);
+  rect.prop('offered', course.offered);
+  rect.addTo(graph);
+  // console.log(rect.toJSON()); // COMMENT OUT THIS LINE TO SEE THE ATTRIBUTES OF THE RECTANGLE
+>>>>>>> c6832e1 (render all courses)
 }
 
 function createStep(x, y, title, color) {
